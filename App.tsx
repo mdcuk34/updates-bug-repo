07cc1cc9c6bc2a2cc2df1f6650d39095dab1408d
Mitch Clay <mdcuk34@gmail.com>
2020-08-04 11:02:34 +0100

    updates bug example

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import * as Updates from 'expo-updates';
+
+const fetchUpdate = async () => {
+    try {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+    } catch(e){
+        alert(e)
+    }
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Updates Test</Text>
+      <Text>Version: {Updates.manifest.version}</Text>
+      <Button onPress={fetchUpdate} title="Fetch Update" />
+      <Image
+          style={styles.image}
+          source={require('./assets/image.png')}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +34,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height: 50,
+    width: 50
+  }
 });
